7771b00ee8c7e5f4071c6a879e98eca72733c4aa
Fangfang Xia <fangfang.xia@gmail.com>
2017-05-01 23:31:32 -0500

    minor update

--- dataframe.py
+++ dataframe.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import os
+import sys
 
 from datasets import nci60
 from argparser import get_parser
@@ -11,9 +12,12 @@ parser = get_parser(description)
 
 parser.add_argument("--by", default='drug', choices=['cell', 'drug'],
                     help='generate dataframes for by cell or by drug problems')
+parser.add_argument("--float_format", default='%.4g',
+                    help='csv float format')
 
 args = parser.parse_args()
 
+sys.stdout = sys.stderr
 print('Args:', args, end='\n\n')
 
 if not os.path.exists(args.out_dir):
@@ -22,14 +26,14 @@ if not os.path.exists(args.out_dir):
 if args.by == 'cell':
     cells = nci60.all_cells() if 'all' in args.cells else args.cells
     for cell in cells:
-        print(cell + ':', end=' ', flush=True)
+        print(cell + ':', end=' ')
         df = nci60.load_by_cell_data(cell, drug_features=args.drug_features, scaling=args.scaling,
                                      min_logconc=args.min_logconc, max_logconc=args.max_logconc,
                                      subsample=args.subsample, feature_subsample=args.feature_subsample,
                                      verbose=False)
         if df.shape[0]:
             fname = os.path.join(args.out_dir, cell + '.csv')
-            df.to_csv(fname)
+            df.to_csv(fname, float_format=args.float_format)
             print('saved {} rows and {} columns to {}'.format(df.shape[0], df.shape[1], os.path.normpath(fname)))
         else:
             print('no response data found')
